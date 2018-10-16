e1b38466f550d07db4471ff7f530076eacd9a874
Corey Sizemore <coreysizemore@gmail.com>
2018-10-16 13:32:35 -0700

    v2.0.7 Theme Updates

---
+++ thegalaxy-child/custom/customelement1.php
@@ -0,0 +1,57 @@
+<?php
+
+	if( have_rows('call_out_boxes','option') ):
+	
+		$items = get_field('youtube_videos');
+		
+		shuffle($items);
+	
+		$count = count($items);
+			
+		echo '<div class="youtube_videos"><div class="container"><div class="row gutters"><div class="col_12">';
+						
+		foreach( $items as $video ):
+				
+			if($count == 1):
+						        
+				echo '<div class="video_1">';
+						
+			elseif($count == 2):
+					
+				echo '<div class="video_2">';
+						
+			elseif($count == 3):
+					
+				echo '<div class="video_3">';
+						
+			elseif($count == 4):
+					
+				echo '<div class="video_4">';
+				
+			elseif($count == 5):
+					
+				echo '<div class="video_5">';
+						
+			else :
+					
+				echo '<div class="video_1">';
+						
+			endif;
+					
+			echo '<div class="youtube_video_item">';
+				
+				echo $video['video_code'];
+					
+			echo '</div></div>';
+						
+		endforeach;
+					
+		echo '</div></div></div></div>';
+							
+	else :
+	
+		// do nothing
+				
+	endif;
+	
+?>
